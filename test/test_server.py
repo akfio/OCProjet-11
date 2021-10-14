345a716ae4883c866f385235e32bcdcec55bc8c1
akfio <ambroise.caffiaux@gmail.com>
2021-10-14 18:32:17 +0200

    Ajout des tests d'integration

--- test/test_server.py
+++ test/test_server.py
@@ -86,11 +86,3 @@ def test_points_display(client, club_fixture):
     expected_points = str(club_fixture['points'])
     assert expected_name.encode() in result.data
     assert expected_points.encode() in result.data
-
-
-def test_points_display_after_purchase(client, club_fixture):
-    data_1 = {'competition': 'Test Event', 'club': club_fixture['name'], 'places': 2}
-    client.post('/purchasePlaces', data=data_1)
-    new_points = str(int(club_fixture["points"]) - int(data_1["places"]))
-    result = client.get('/points')
-    assert new_points.encode() in result.data
